33ac8db9c4bb453dd2ebb78527f04ef4b756434e
Willian Garcia <webwil2012@gmail.com>
2024-10-25 07:35:42 -0300

    Tipos de operadores

---
+++ python/02-tipo-de-operadores/operadores_aritimeticos.py
@@ -0,0 +1,10 @@
+produto_01 = 10
+produto_02 = 20
+
+print(produto_02 + produto_01)
+print(produto_02 - produto_01)
+print(produto_02 / produto_01)
+print(produto_02 // produto_01)
+print(produto_02 * produto_01)
+print(produto_02 % produto_01)
+print(produto_02 ** produto_01)
